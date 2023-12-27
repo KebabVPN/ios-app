94318ff5e99a20ac08d9fb5dbab328f9e2947319
Eugene Khashin <adastreamer@gmail.com>
2023-12-27 17:47:49 -0500

    Initial Commit

---
+++ kebab/kebabApp.swift
@@ -0,0 +1,17 @@
+//
+//  kebabApp.swift
+//  kebab
+//
+//  Created by Evgeniy Khashin on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct kebabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
