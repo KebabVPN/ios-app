50caf92a08a6b887d207035ea57205fa94610ffc
dobrys1abar <117594554+dobrys1abar@users.noreply.github.com>
2024-03-06 12:34:10 +0300

    macOS support added, fixed cancellation process

--- kebab/lib/LoginParams.swift
+++ kebab/lib/LoginParams.swift
@@ -8,8 +8,6 @@
 import Foundation
 
 struct Params {
-//    let username = "<#username#>"
-//    let password = "<#simplepassword#>"
-    let username = "1465974653"
-    let password = "b322448b39e08e3f55c1905156951601"
+    let username = "<#username#>"
+    let password = "<#simplepassword#>"
 }
